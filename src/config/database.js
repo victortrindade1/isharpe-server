495f4893f854fa40233843e564ed2927682891f6
Victor Trindade <victortrindade@gmail.com>
2023-12-24 07:16:16 -0300

    Git init - Node server + Express + Postgres + Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "123456",
+  database: "isharpe",
+  port: "5433",
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
---
+++ src/database/migrations/20231223120059-create-users.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  },
+};
